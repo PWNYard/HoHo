f3a38d2ff58f6c76a3992e5f03284176b7d78a28
PWNYard <sheydshaydin@gmail.com>
2025-01-28 13:05:54 -0800

    Update PWN.lua

--- PWN.lua
+++ PWN.lua
@@ -1,18 +1,18 @@
-local hoho_uis = 0;
-spawn(function()
-    while task.wait(35) do
-        for i,v in pairs(game:GetService("CoreGui"):GetChildren()) do
-            if v["Name"] == "Hоhо_gеn4" then
-                hoho_uis += 1;
-            end;
-        end;
-        if hoho_uis ~= 2 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/PWNYard/HoHo/refs/heads/main/MainComponent.lua"))();
-        else
-          break;
-        end;
-        hoho_uis = 0;
-    end;
-end)
+-- local hoho_uis = 0;
+-- spawn(function()
+--     while task.wait(35) do
+--         for i,v in pairs(game:GetService("CoreGui"):GetChildren()) do
+--             if v["Name"] == "Hоhо_gеn4" then
+--                 hoho_uis += 1;
+--             end;
+--         end;
+--         if hoho_uis ~= 2 then
+--             loadstring(game:HttpGet("https://raw.githubusercontent.com/PWNYard/HoHo/refs/heads/main/MainComponent.lua"))();
+--         else
+--           break;
+--         end;
+--         hoho_uis = 0;
+--     end;
+-- end)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PWNYard/HoHo/refs/heads/main/MainComponent.lua"))();
